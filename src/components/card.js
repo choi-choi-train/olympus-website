8dadaba12301822d22901b0afbe29ed91e28d397
Tom Choi <tomchoi2025@u.northwestern.edu>
2023-01-18 21:09:42 -0600

    1/18/23 Update

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { isMobile } from 'react-device-detect';
 
 export default function CARD(props) {
   var calc = (props.scrollPosition - props.scrollStart) / 3;
   var o = calc.toFixed(2) + "%"
 
   return (
-    <div className="card mb-3">
+    isMobile? 
+    <div className="d-flex flex-row justify-start mb-2" style={{width: '90%'}}>
+      <img className="card-key" src={require('../assets/grecian_key_down.png')} alt="key"
+        style={{marginRight: '10px', width:'15px', height:'15px'}}/>
+      <div className="d-flex flex-column justify-start">
+        <h4 className="gold" style={{zIndex:2, marginTop:'3px'}}>
+          {props.title}
+        </h4>
+        <h6 className="fw-light" style={{zIndex:2}}>
+            {props.content}
+        </h6>
+      </div>
+    </div>
+    : <div className="card mb-3">
         <h4 className="gold" style={{zIndex:2}}>
             {props.title}
         </h4>
--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ export default function footer() {
   return (
     <div className="container-fluid d-flex flex-row justify-content-end align-items-end
                     footer padded">
-        <h6 className='fw-bold black'>OLYMPUS 2022</h6>
+        <h6 className='fw-bold black'>OLYMPUS {new Date().getFullYear()}</h6>
     </div>
   )
 }
